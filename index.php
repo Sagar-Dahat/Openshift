06bbbd53fa19df5f7103b94c14a0e7a298e714b1
Sagar Dahat <65307048+Sagar-Dahat@users.noreply.github.com>
2021-03-11 22:08:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-print  "Hello this is Openshift Development ...(4th Commit)"
-print  "This is second line"
+print  "Hello this is Openshift Development ...(4th Commit)
+This is the second line";
 
 ?>
